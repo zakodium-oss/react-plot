bcc44d6efd89bc0e16ee08c84d730e47d8afa189
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-10-22 14:41:15 +0200

    chore: initial structure

---
+++ README.md
@@ -0,0 +1,21 @@
+# react-2dplot
+
+[![NPM version][npm-image]][npm-url]
+[![npm download][download-image]][download-url]
+
+Render 2dplot partition in react.
+
+## [Storybook](https://zakodium.github.io/react-2dplot/)
+
+## Installation
+
+`$ npm install --save react-2dplot`
+
+## License
+
+[MIT](./LICENSE)
+
+[npm-image]: https://img.shields.io/npm/v/react-2dplot.svg?style=flat-square
+[npm-url]: https://www.npmjs.com/package/react-2dplot
+[download-image]: https://img.shields.io/npm/dm/react-2dplot.svg?style=flat-square
+[download-url]: https://www.npmjs.com/package/react-2dplot
---
+++ stories/plot.stories.tsx
@@ -0,0 +1,32 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+
+import { Plot } from '../src/Plot';
+
+export default {
+  title: 'Example/Plot',
+  component: Plot,
+  argTypes: {
+    rows: {
+      defaultValue: '6',
+      control: 'number',
+    },
+    columns: {
+      defaultValue: '7',
+      control: 'number',
+    },
+  },
+} as Meta;
+
+export function Control(props) {
+  const pickedItems = [{ index: '3' }, { index: '5', label: 'owner' }];
+  return (
+    <Plot
+      pickedItems={pickedItems}
+      size={320}
+      // eslint-disable-next-line no-console
+      onSelect={console.log}
+      {...props}
+    />
+  );
+}
