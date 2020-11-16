7c3a616d2a0453066223efe1e62c5a7ae437a5d5
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-11-16 18:35:25 +0100

    feat: change state identification

--- src/utils.ts
+++ src/utils.ts
@@ -3,18 +3,6 @@ import React from 'react';
 import LineSeries from './components/LineSeries';
 import type { PlotChildren } from './types';
 
-export function getMax(actionMax: number, stateMax: number | null | undefined) {
-  return stateMax === null || stateMax === undefined || stateMax <= actionMax
-    ? actionMax
-    : stateMax;
-}
-
-export function getMin(actionMin: number, stateMin: number | null | undefined) {
-  return stateMin === null || stateMin === undefined || stateMin >= actionMin
-    ? actionMin
-    : stateMin;
-}
-
 export function splitChildren(children: React.ReactNode[]): PlotChildren {
   let invalidChild = false;
   let lineSeries = [];
