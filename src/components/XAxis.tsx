d260dc26382a450962fa49afd58d02d05500aa93
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2020-11-17 17:30:39 +0100

    fix: avoid side-effects directly in render

--- src/components/XAxis.tsx
+++ src/components/XAxis.tsx
@@ -1,6 +1,6 @@
 import { axisBottom } from 'd3-axis';
 import { select } from 'd3-selection';
-import React, { useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 
 import { usePlotContext } from '../hooks';
 import type { AxisProps } from '../types';
@@ -8,30 +8,34 @@ import type { AxisProps } from '../types';
 const XAxis = ({ label, fontSize = 16, showTicks }: AxisProps) => {
   const axisRef = useRef(null);
   const { xScale, margin, height, width } = usePlotContext();
-  if (axisRef?.current) {
-    const axis = axisBottom(xScale);
-    if (showTicks) {
-      axis.tickSizeInner((margin?.bottom || 0) + (margin?.top || 0) - height);
-    }
 
-    select(axisRef.current)
-      .call(axis)
-      .call((g) => {
-        g.selectAll('.tick line')
-          .attr('stroke-opacity', showTicks ? 0.5 : 1)
-          .attr('stroke-dasharray', showTicks ? '2,2' : '0');
-        g.selectAll('.tick text').attr(
-          'transform',
-          `translate(0,${showTicks ? 6 : 0})`,
-        );
-      });
-  }
+  useLayoutEffect(() => {
+    if (axisRef?.current) {
+      const axis = axisBottom(xScale);
+      if (showTicks) {
+        axis.tickSizeInner((margin?.bottom || 0) + (margin?.top || 0) - height);
+      }
+
+      select(axisRef.current)
+        .call(axis)
+        .call((g) => {
+          g.selectAll('.tick line')
+            .attr('stroke-opacity', showTicks ? 0.5 : 1)
+            .attr('stroke-dasharray', showTicks ? '2,2' : '0');
+          g.selectAll('.tick text').attr(
+            'transform',
+            `translate(0,${showTicks ? 6 : 0})`,
+          );
+        });
+    }
+  });
 
   // Recomend bigger margin
   if ((margin?.left || 0) < fontSize + 20) {
     // eslint-disable-next-line no-console
     console.warn(`Your margin left should be at least ${fontSize + 20}`);
   }
+
   return (
     <>
       <g
--- src/components/YAxis.tsx
+++ src/components/YAxis.tsx
@@ -1,6 +1,6 @@
 import { axisLeft } from 'd3-axis';
 import { select } from 'd3-selection';
-import React, { useRef } from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 
 import { usePlotContext } from '../hooks';
 import type { AxisProps } from '../types';
@@ -8,29 +8,33 @@ import type { AxisProps } from '../types';
 const YAxis = ({ label, fontSize = 16, showTicks }: AxisProps) => {
   const axisRef = useRef(null);
   const { yScale, margin, height, width } = usePlotContext();
-  if (axisRef?.current) {
-    const axis = axisLeft(yScale);
-    if (showTicks) {
-      axis.tickSizeInner((margin?.left || 0) + (margin?.right || 0) - width);
+
+  useLayoutEffect(() => {
+    if (axisRef?.current) {
+      const axis = axisLeft(yScale);
+      if (showTicks) {
+        axis.tickSizeInner((margin?.left || 0) + (margin?.right || 0) - width);
+      }
+      select(axisRef.current)
+        .call(axis)
+        .call((g) => {
+          g.selectAll('.tick line')
+            .attr('stroke-opacity', showTicks ? 0.5 : 1)
+            .attr('stroke-dasharray', showTicks ? '2,2' : '0');
+          g.selectAll('.tick text').attr(
+            'transform',
+            `translate(${showTicks ? -6 : 0},0)`,
+          );
+        });
     }
-    select(axisRef.current)
-      .call(axis)
-      .call((g) => {
-        g.selectAll('.tick line')
-          .attr('stroke-opacity', showTicks ? 0.5 : 1)
-          .attr('stroke-dasharray', showTicks ? '2,2' : '0');
-        g.selectAll('.tick text').attr(
-          'transform',
-          `translate(${showTicks ? -6 : 0},0)`,
-        );
-      });
-  }
+  });
 
   // Recomend bigger margin
   if ((margin?.bottom || 0) < fontSize + 30) {
     // eslint-disable-next-line no-console
     console.warn(`Your margin bottom should be at least ${fontSize + 30}`);
   }
+
   return (
     <>
       <g ref={axisRef} transform={`translate(${margin?.left || 0}, 0)`} />
