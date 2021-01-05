842c66bded362938b044064ee9d1b631893c1388
Miguel Angel Asencio Hurtado <maasencioh@gmail.com>
2021-01-05 15:31:21 +0100

    chore: changing API from{x:[],y:[]} to [{x,y}]
    BREAKING CHANGE

--- src/components/LineSeries.tsx
+++ src/components/LineSeries.tsx
@@ -1,6 +1,6 @@
 import { extent } from 'd3-array';
 import { line } from 'd3-shape';
-import React, { CSSProperties, useEffect, useMemo, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import { useDispatchContext, usePlotContext } from '../hooks';
 import type { LineSeriesProps, Series } from '../types';
@@ -8,13 +8,8 @@ import { getNextId } from '../utils';
 
 import { Circle, Square, Triangle } from './Markers';
 
-interface LineSeriesRenderProps {
+interface LineSeriesRenderProps extends Omit<LineSeriesProps, 'label'> {
   id: string;
-  data: Series;
-  lineStyle?: CSSProperties;
-  displayMarker?: boolean;
-  markerShape?: 'circle' | 'square' | 'triangle';
-  markerSize?: number;
 }
 
 const markersComps = {
@@ -31,8 +26,8 @@ export default function LineSeries(props: LineSeriesProps) {
   // Update plot context with data description
   const { dispatch } = useDispatchContext();
   useEffect(() => {
-    const [xMin, xMax] = extent(data.x);
-    const [yMin, yMax] = extent(data.y);
+    const [xMin, xMax] = extent(data, (d) => d.x);
+    const [yMin, yMax] = extent(data, (d) => d.y);
     dispatch({ type: 'newData', value: { id, xMin, xMax, yMin, yMax, label } });
 
     // Delete information on unmount
@@ -57,31 +52,20 @@ function LineSeriesRender({
   // calculates the path to display
   const color = colorScaler(id);
   const [path, markers] = useMemo(() => {
-    if (
-      [xScale, yScale].includes(undefined) ||
-      data.x.length !== data.y.length
-    ) {
+    if ([xScale, yScale].includes(undefined)) {
       return [null, null];
     }
 
-    // Format data for line function generator
-    let series = [];
-    for (let index = 0; index < data.x.length; index++) {
-      const x = xScale(data.x[index]);
-      const y = yScale(data.y[index]);
-      series.push({ x, y });
-    }
-
     // Calculate line from D3
-    const lineGenerator = line<{ x: number; y: number }>()
-      .x((d) => d.x)
-      .y((d) => d.y);
+    const lineGenerator = line<Series>()
+      .x((d) => xScale(d.x))
+      .y((d) => yScale(d.y));
 
     // Show markers
     const Marker = markersComps[markerShape];
     const markers = !displayMarker
       ? null
-      : series.map(({ x, y }, i) => (
+      : data.map(({ x, y }, i) => (
           <Marker
             // eslint-disable-next-line react/no-array-index-key
             key={`markers-${i}`}
@@ -92,7 +76,7 @@ function LineSeriesRender({
           />
         ));
 
-    return [lineGenerator(series), markers];
+    return [lineGenerator(data), markers];
   }, [xScale, yScale, color, data, displayMarker, markerSize, markerShape]);
   if ([xScale, yScale].includes(null)) return null;
 
