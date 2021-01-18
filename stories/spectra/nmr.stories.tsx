d3363048d30de25be8fd55e52448e16fff165b59
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2021-01-18 09:49:41 +0100

    docs: implement NMR example

---
+++ stories/spectra/nmr.stories.tsx
@@ -0,0 +1,35 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+
+import { Axis, LineSeries, Plot } from '../../src';
+import data from '../data/nmr.json';
+
+export default {
+  title: 'Experimental spectra/NMR',
+} as Meta;
+
+export function NmrExample() {
+  return (
+    <Plot
+      width={1400}
+      height={540}
+      margin={{ bottom: 45, left: 40, top: 40, right: 40 }}
+      viewportStyle={{ stroke: 'black' }}
+    >
+      <LineSeries
+        data={data}
+        xAxis="x"
+        yAxis="y"
+        lineStyle={{ stroke: 'black' }}
+      />
+      <Axis id="x" position="bottom" label="δ [ppm]" display flip />
+      <Axis
+        id="y"
+        position="left"
+        label="Intensity / arbitrary"
+        display
+        padding={[0.1, 0.1]}
+      />
+    </Plot>
+  );
+}
