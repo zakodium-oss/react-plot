3dfb17263bdfcc7a2a64a9760dfc2c3431715b0d
NadjibSb <fn_souab@esi.dz>
2021-01-22 22:10:26 +0100

    docs: add 'COVID19 cases' example

---
+++ stories/examples/covid-cases.stories.tsx
@@ -0,0 +1,39 @@
+import { Meta } from '@storybook/react';
+import React from 'react';
+
+import { Axis, LineSeries, Plot, Heading } from '../../src';
+import data from '../data/covid-cases.json';
+
+export default {
+  title: 'Examples/Covid19 cases-USA',
+} as Meta;
+
+export function Covid19Cases() {
+  return (
+    <Plot
+      width={1400}
+      height={540}
+      margin={{ bottom: 50, left: 100, top: 50, right: 10 }}
+      viewportStyle={{
+        stroke: 'black',
+        strokeWidth: 0.3,
+      }}
+    >
+      <Heading title="COVID-19 cases in USA (2020)" />
+      <LineSeries
+        data={data}
+        xAxis="x"
+        yAxis="y"
+        lineStyle={{ stroke: 'red', strokeWidth: 2 }}
+      />
+      <Axis id="x" position="bottom" label="Week" labelSpace={30} />
+      <Axis
+        id="y"
+        position="left"
+        label="Number of cases"
+        labelSpace={68}
+        paddingEnd={0.1}
+      />
+    </Plot>
+  );
+}
