5600653d91a9b00cde7dc9afd81583c678926743
Sebastien Ahkrin <sebastien48criquet@gmail.com>
2021-03-16 11:21:32 +0100

    refactor: remove every import of React

--- src/components/Annotations/Line.tsx
+++ src/components/Annotations/Line.tsx
@@ -1,4 +1,4 @@
-import React, { SVGProps } from 'react';
+import { SVGProps } from 'react';
 
 type LineProps = SVGProps<SVGLineElement>;
 
--- src/components/Annotations/Rectangle.tsx
+++ src/components/Annotations/Rectangle.tsx
@@ -1,4 +1,4 @@
-import React, { SVGProps } from 'react';
+import { SVGProps } from 'react';
 
 import { usePosition } from '../../hooks';
 
