d73eb60315701cf6f1041b011faab16127f9f177
Luc Patiny <luc@patiny.com>
2021-04-14 11:57:59 +0200

    docs: add experimental mass example

---
+++ stories/spectra/mass.stories.tsx
@@ -0,0 +1,34 @@
+import { Meta } from '@storybook/react';
+
+import { Axis, BarSeries, Plot } from '../../src';
+import data from '../data/mass.json';
+import { DEFAULT_CONFIG } from '../utils';
+
+export default {
+  title: 'Experimental spectra/Mass',
+} as Meta;
+
+export function MassExample() {
+  return (
+    <Plot
+      {...DEFAULT_CONFIG}
+      margin={{ bottom: 45, left: 40, top: 40, right: 40 }}
+      seriesViewportStyle={{ stroke: 'black' }}
+    >
+      <BarSeries
+        data={data}
+        xAxis="x"
+        yAxis="y"
+        lineStyle={{ stroke: 'blue' }}
+      />
+
+      <Axis id="x" position="bottom" label="Mass [m/z]" />
+      <Axis
+        id="y"
+        position="left"
+        label="Relative intensity [%]"
+        paddingEnd={0.1}
+      />
+    </Plot>
+  );
+}
