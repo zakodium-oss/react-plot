64525b8cae18271faacb09d3b57d494bc8fecc0c
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-09 22:43:28 +0100

    fix : delete doble json file

--- stories/spectra/mass.stories.tsx
+++ stories/spectra/mass.stories.tsx
@@ -1,7 +1,7 @@
 import { Meta } from '@storybook/react';
 
+import advancedData from '../../scripts/mass/HCys100OH_0.01.json';
 import { Axis, BarSeries, LineSeries, Plot } from '../../src';
-import advancedData from '../data/advenced-mass.json';
 import data from '../data/mass.json';
 import { DEFAULT_PLOT_CONFIG } from '../utils';
 
