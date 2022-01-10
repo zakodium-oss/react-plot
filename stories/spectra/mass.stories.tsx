06b65aeec4c0fdb365acba40419aeeda713570d0
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-10 15:27:27 +0100

    feat: add best peaks lable

--- stories/spectra/mass.stories.tsx
+++ stories/spectra/mass.stories.tsx
@@ -1,6 +1,8 @@
 import { Meta } from '@storybook/react';
 
-import { Axis, BarSeries, LineSeries, Plot } from '../../src';
+import { Annotations, Axis, BarSeries, LineSeries, Plot } from '../../src';
+import { Line } from '../../src/components/Annotations/Line';
+import { Text } from '../../src/components/Annotations/Text';
 import advancedData from '../data/HCys100OH_0.01.json';
 import data from '../data/mass.json';
 import { DEFAULT_PLOT_CONFIG } from '../utils';
@@ -43,7 +45,35 @@ export function AdvancedMassExample() {
         yAxis="y"
         lineStyle={{ stroke: 'red' }}
       />
-      <Axis id="x" position="bottom" label="Mass [m/z]" />
+      <Annotations>
+        {advancedData.bestPeaks.map((peak) => (
+          <>
+            <Line
+              key={peak.label}
+              x1={peak.x}
+              x2={peak.x}
+              y1={peak.y}
+              y2={peak.y + 2}
+              style={{ stroke: 'blue' }}
+            />
+            <Text
+              style={{ fontSize: '13px', fontWeight: '600' }}
+              x={peak.x}
+              y={peak.y}
+            >
+              {peak.shortLabel}
+            </Text>
+            <Text
+              style={{ fontSize: '13px', fontWeight: '600' }}
+              x={peak.x}
+              y={peak.y + 3}
+            >
+              {peak.x.toFixed(4)}
+            </Text>
+          </>
+        ))}
+      </Annotations>
+      <Axis min={10310} id="x" position="bottom" label="Mass [m/z]" />
       <Axis
         id="y"
         position="left"
