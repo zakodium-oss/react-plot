718d4e6d03f27341a0a610b533c2c44c8a3276df
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-11 17:32:20 +0100

    feat : add color prop to line

--- src/components/Annotations/Line.tsx
+++ src/components/Annotations/Line.tsx
@@ -8,6 +8,7 @@ export interface AnnotationLineProps
   x2: number | string;
   y1: number | string;
   y2: number | string;
+  color: string;
 }
 
 export function Line(props: AnnotationLineProps) {
@@ -16,12 +17,10 @@ export function Line(props: AnnotationLineProps) {
     x2: oldX2,
     y1: oldY1,
     y2: oldY2,
-    stroke = 'black',
+    color = 'black',
     ...lineProps
   } = props;
   const { x: x1, y: y1 } = usePosition({ x: oldX1, y: oldY1 });
   const { x: x2, y: y2 } = usePosition({ x: oldX2, y: oldY2 });
-  return (
-    <line x1={x1} x2={x2} y1={y1} y2={y2} stroke={stroke} {...lineProps} />
-  );
+  return <line x1={x1} x2={x2} y1={y1} y2={y2} stroke={color} {...lineProps} />;
 }
