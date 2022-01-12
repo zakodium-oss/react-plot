9466eb340d65ca0d5f055981409ca7fb036a12e9
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-12 12:43:48 +0100

    fix: shape

--- src/components/Annotations/Shape.tsx
+++ src/components/Annotations/Shape.tsx
@@ -20,43 +20,22 @@ export type AnnotationShapeName = 'triangle' | 'circle' | 'diamond' | 'square';
 interface IntAnnotationShape {
   size: number;
   style?: CSSProperties;
-  color?: string;
 }
 
 function Triangle(props: IntAnnotationShape) {
-  return (
-    <TriangleMarker
-      size={props.size}
-      style={{ fill: props.color, ...props.style }}
-    />
-  );
+  return <TriangleMarker size={props.size} style={props.style} />;
 }
 
 function Circle(props: IntAnnotationShape) {
-  return (
-    <CircleMarker
-      size={props.size}
-      style={{ fill: props.color, ...props.style }}
-    />
-  );
+  return <CircleMarker size={props.size} style={props.style} />;
 }
 
 function Diamond(props: IntAnnotationShape) {
-  return (
-    <DiamondMarker
-      size={props.size}
-      style={{ fill: props.color, ...props.style }}
-    />
-  );
+  return <DiamondMarker size={props.size} style={props.style} />;
 }
 
 function Square(props: IntAnnotationShape) {
-  return (
-    <SquareMarker
-      size={props.size}
-      style={{ fill: props.color, ...props.style }}
-    />
-  );
+  return <SquareMarker size={props.size} style={props.style} />;
 }
 
 export interface AnnotationShapeProps {
@@ -79,7 +58,7 @@ export function Shape(props: AnnotationShapeProps) {
 
   return (
     <g transform={`translate(${x}, ${y})`}>
-      <Figure size={props.size} style={props.style} color={props.color} />
+      <Figure size={props.size} style={{ fill: props.color, ...props.style }} />
     </g>
   );
 }
