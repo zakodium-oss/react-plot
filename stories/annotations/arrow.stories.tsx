989ad21cb57e73e9e6f3cc3c873abe6d5e2046c2
Wadjih Bencheikh <56769876+wadjih-bencheikh18@users.noreply.github.com>
2022-01-13 16:41:44 +0100

    feat: add color prop to annotation arrow  (#231)

--- stories/annotations/arrow.stories.tsx
+++ stories/annotations/arrow.stories.tsx
@@ -17,6 +17,7 @@ export default {
     y2: 33,
     startPoint: 'none',
     endPoint: 'triangle',
+    color: 'red',
   },
 } as Meta<AnnotationArrowProps>;
 
@@ -24,6 +25,16 @@ export function AnnotationArrow(props: AnnotationArrowProps) {
   return (
     <AnnotationPlot>
       <Arrow {...props} />
+
+      <Arrow
+        x1="5"
+        y1="5"
+        x2="50"
+        y2="22"
+        endPoint="none"
+        startPoint="triangle"
+        color="blue"
+      />
     </AnnotationPlot>
   );
 }
