34e67ef28b54356524b7c152c0dbae9fdb7d1e97
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-15 15:30:16 +0100

    docs : fix bar Story

--- stories/control/bar.stories.tsx
+++ stories/control/bar.stories.tsx
@@ -7,86 +7,52 @@ export default {
   title: 'API/BarSeries',
   component: BarSeries,
   argTypes: {
-    displayMarker: {
-      control: 'boolean',
-      defaultValue: true,
-    },
-    lineStyle: {
-      control: 'object',
-      defaultValue: {
-        stroke: 'black',
-      },
-    },
-    markerStyle: {
-      control: 'object',
-      defaultValue: { fill: 'black' },
-    },
-    hidden: {
-      control: 'boolean',
-      defaultValue: false,
-    },
-    label: {
-      control: 'text',
-      defaultValue: 'Label',
-    },
-    markerShape: {
-      defaultValue: 'circle',
-    },
-    markerSize: {
-      control: 'number',
-      defaultValue: 10,
-    },
-    groupId: {
-      table: {
-        disable: true,
-      },
-    },
-    xAxis: {
-      table: {
-        disable: true,
-      },
-    },
-    yAxis: {
-      table: {
-        disable: true,
-      },
-    },
     data: {
       table: {
         disable: true,
       },
     },
   },
-} as Meta;
-
-const data = [
-  {
-    x: 0,
-    y: 10,
+  args: {
+    displayMarker: true,
+    lineStyle: {
+      stroke: 'black',
+    },
+    markerStyle: { fill: 'black' },
+    hidden: false,
+    label: 'Label',
+    markerShape: 'circle',
+    markerSize: 10,
+    data: [
+      {
+        x: 0,
+        y: 10,
+      },
+      {
+        x: 1,
+        y: 12,
+      },
+      {
+        x: 2,
+        y: 14,
+      },
+      {
+        x: 3,
+        y: 16,
+      },
+      {
+        x: 4,
+        y: 18,
+      },
+    ],
   },
-  {
-    x: 1,
-    y: 12,
-  },
-  {
-    x: 2,
-    y: 14,
-  },
-  {
-    x: 3,
-    y: 16,
-  },
-  {
-    x: 4,
-    y: 18,
-  },
-];
+} as Meta<BarSeriesProps>;
 
 export function Control(props: BarSeriesProps) {
   return (
     <Plot {...DEFAULT_PLOT_CONFIG}>
-      <BarSeries {...props} data={data} xAxis="x" yAxis="y" />
-      <Axis id="x" position="bottom" />
+      <BarSeries {...props} xAxis="x" yAxis="y" />
+      <Axis id="x" position="bottom" paddingStart={0.1} paddingEnd={0.1} />
       <Axis id="y" position="left" paddingStart={0.1} paddingEnd={0.1} />
 
       <Legend position="embedded" />
