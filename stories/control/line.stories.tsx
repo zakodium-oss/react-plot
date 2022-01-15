e4145e99efdd852e96cae44fe3e096961215254d
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-15 15:34:14 +0100

    docs : line story

--- stories/control/line.stories.tsx
+++ stories/control/line.stories.tsx
@@ -1,136 +1,72 @@
 import { Meta } from '@storybook/react';
 
-import {
-  Axis,
-  Legend,
-  LineSeries,
-  Plot,
-  LineSeriesProps,
-  SeriesPoint,
-} from '../../src';
+import { Axis, Legend, LineSeries, Plot, LineSeriesProps } from '../../src';
 import { DEFAULT_PLOT_CONFIG } from '../utils';
 
 export default {
   title: 'API/LineSeries',
   component: LineSeries,
-  argTypes: {
-    displayMarker: {
-      control: 'boolean',
-      defaultValue: true,
-    },
+  args: {
+    displayMarker: true,
     lineStyle: {
-      control: 'object',
-      defaultValue: {
-        stroke: 'black',
-      },
+      stroke: 'black',
     },
-    markerStyle: {
-      control: 'object',
-      defaultValue: { fill: 'black' },
-    },
-    hidden: {
-      control: 'boolean',
-      defaultValue: false,
-    },
-    label: {
-      control: 'text',
-      defaultValue: 'Label',
-    },
-    markerShape: {
-      defaultValue: 'circle',
-    },
-    markerSize: {
-      control: 'number',
-      defaultValue: 10,
-    },
-    // ErrorBars props
-    displayErrorBars: {
-      control: 'boolean',
-      defaultValue: true,
-      table: {
-        category: 'Error Bars',
-      },
-    },
-    errorBarsCapSize: {
-      control: 'number',
-      defaultValue: 10,
-      table: {
-        category: 'Error Bars',
-      },
-    },
-    errorBarsStyle: {
-      defaultValue: { strokeWidth: 1 },
-      table: {
-        category: 'Error Bars',
-      },
-    },
-    errorBarsCapStyle: {
-      defaultValue: { stroke: 'blue' },
-      table: {
-        category: 'Error Bars',
-      },
-    },
-    // Disable unnecessary controls
-    groupId: {
-      table: {
-        disable: true,
-      },
-    },
-    xAxis: {
-      table: {
-        disable: true,
-      },
-    },
-    yAxis: {
-      table: {
-        disable: true,
-      },
-    },
-    data: {
-      table: {
-        disable: true,
-      },
-    },
-  },
-} as Meta;
+    markerStyle: { fill: 'black' },
 
-const data: SeriesPoint[] = [
-  {
-    x: 0,
-    y: 10,
-    xError: 0.2,
-    yError: 1,
+    hidden: false,
+    label: 'Label',
+
+    markerShape: 'circle',
+
+    markerSize: 10,
+    // ErrorBars props
+    displayErrorBars: true,
+
+    errorBarsCapSize: 10,
+    errorBarsStyle: { strokeWidth: 1 },
+
+    errorBarsCapStyle: { stroke: 'blue' },
+
+    // Disable unnecessary controls
+    data: [
+      {
+        x: 0,
+        y: 10,
+        xError: 0.2,
+        yError: 1,
+      },
+      {
+        x: 1,
+        y: 12,
+        xError: [0.1, 0.1],
+        yError: [0.5, 0.5],
+      },
+      {
+        x: 2,
+        y: 14,
+        xError: [0.2, null],
+        yError: [0, 0.5],
+      },
+      {
+        x: 3,
+        y: 16,
+        xError: [0.1, 0.2],
+        yError: null,
+      },
+      {
+        x: 4,
+        y: 18,
+        xError: 0.2,
+        yError: 0.5,
+      },
+    ],
   },
-  {
-    x: 1,
-    y: 12,
-    xError: [0.1, 0.1],
-    yError: [0.5, 0.5],
-  },
-  {
-    x: 2,
-    y: 14,
-    xError: [0.2, null],
-    yError: [0, 0.5],
-  },
-  {
-    x: 3,
-    y: 16,
-    xError: [0.1, 0.2],
-    yError: null,
-  },
-  {
-    x: 4,
-    y: 18,
-    xError: 0.2,
-    yError: 0.5,
-  },
-];
+} as Meta<LineSeriesProps>;
 
 export function Control(props: LineSeriesProps) {
   return (
     <Plot {...DEFAULT_PLOT_CONFIG}>
-      <LineSeries {...props} data={data} xAxis="x" yAxis="y" />
+      <LineSeries {...props} xAxis="x" yAxis="y" />
       <Axis id="x" position="bottom" />
       <Axis id="y" position="left" paddingStart={0.1} paddingEnd={0.1} />
 
