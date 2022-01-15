8b9e7b32b233160df923d6caa25675707a41a6e9
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-15 15:39:33 +0100

    fix : data in bar & line

--- stories/control/bar.stories.tsx
+++ stories/control/bar.stories.tsx
@@ -6,13 +6,6 @@ import { DEFAULT_PLOT_CONFIG } from '../utils';
 export default {
   title: 'API/BarSeries',
   component: BarSeries,
-  argTypes: {
-    data: {
-      table: {
-        disable: true,
-      },
-    },
-  },
   args: {
     displayMarker: true,
     lineStyle: {
@@ -23,35 +16,34 @@ export default {
     label: 'Label',
     markerShape: 'circle',
     markerSize: 10,
-    data: [
-      {
-        x: 0,
-        y: 10,
-      },
-      {
-        x: 1,
-        y: 12,
-      },
-      {
-        x: 2,
-        y: 14,
-      },
-      {
-        x: 3,
-        y: 16,
-      },
-      {
-        x: 4,
-        y: 18,
-      },
-    ],
   },
 } as Meta<BarSeriesProps>;
-
+const data = [
+  {
+    x: 0,
+    y: 10,
+  },
+  {
+    x: 1,
+    y: 12,
+  },
+  {
+    x: 2,
+    y: 14,
+  },
+  {
+    x: 3,
+    y: 16,
+  },
+  {
+    x: 4,
+    y: 18,
+  },
+];
 export function Control(props: BarSeriesProps) {
   return (
     <Plot {...DEFAULT_PLOT_CONFIG}>
-      <BarSeries {...props} xAxis="x" yAxis="y" />
+      <BarSeries {...props} data={data} xAxis="x" yAxis="y" />
       <Axis id="x" position="bottom" paddingStart={0.1} paddingEnd={0.1} />
       <Axis id="y" position="left" paddingStart={0.1} paddingEnd={0.1} />
 
--- stories/control/line.stories.tsx
+++ stories/control/line.stories.tsx
@@ -1,6 +1,13 @@
 import { Meta } from '@storybook/react';
 
-import { Axis, Legend, LineSeries, Plot, LineSeriesProps } from '../../src';
+import {
+  Axis,
+  Legend,
+  LineSeries,
+  Plot,
+  LineSeriesProps,
+  SeriesPoint,
+} from '../../src';
 import { DEFAULT_PLOT_CONFIG } from '../utils';
 
 export default {
@@ -26,47 +33,44 @@ export default {
     errorBarsStyle: { strokeWidth: 1 },
 
     errorBarsCapStyle: { stroke: 'blue' },
-
-    // Disable unnecessary controls
-    data: [
-      {
-        x: 0,
-        y: 10,
-        xError: 0.2,
-        yError: 1,
-      },
-      {
-        x: 1,
-        y: 12,
-        xError: [0.1, 0.1],
-        yError: [0.5, 0.5],
-      },
-      {
-        x: 2,
-        y: 14,
-        xError: [0.2, null],
-        yError: [0, 0.5],
-      },
-      {
-        x: 3,
-        y: 16,
-        xError: [0.1, 0.2],
-        yError: null,
-      },
-      {
-        x: 4,
-        y: 18,
-        xError: 0.2,
-        yError: 0.5,
-      },
-    ],
   },
 } as Meta<LineSeriesProps>;
-
+const data: SeriesPoint[] = [
+  {
+    x: 0,
+    y: 10,
+    xError: 0.2,
+    yError: 1,
+  },
+  {
+    x: 1,
+    y: 12,
+    xError: [0.1, 0.1],
+    yError: [0.5, 0.5],
+  },
+  {
+    x: 2,
+    y: 14,
+    xError: [0.2, null],
+    yError: [0, 0.5],
+  },
+  {
+    x: 3,
+    y: 16,
+    xError: [0.1, 0.2],
+    yError: null,
+  },
+  {
+    x: 4,
+    y: 18,
+    xError: 0.2,
+    yError: 0.5,
+  },
+];
 export function Control(props: LineSeriesProps) {
   return (
     <Plot {...DEFAULT_PLOT_CONFIG}>
-      <LineSeries {...props} xAxis="x" yAxis="y" />
+      <LineSeries {...props} data={data} xAxis="x" yAxis="y" />
       <Axis id="x" position="bottom" />
       <Axis id="y" position="left" paddingStart={0.1} paddingEnd={0.1} />
 
