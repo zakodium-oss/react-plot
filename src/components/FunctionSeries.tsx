071ec6d239bfde9dd2608a257a5304620ffd9cfe
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-01-31 17:41:13 +0100

    fix :fonctionSerie

--- src/components/FunctionSeries.tsx
+++ src/components/FunctionSeries.tsx
@@ -19,20 +19,24 @@ export function FunctionSeries(props: FunctionSeriesProps) {
     plotHeight,
   } = usePlotContext();
   const step = 1;
-  const isHorizontal = x
-    ? x.position === 'top' || x.position === 'bottom'
-    : undefined;
-  const min = x ? toNumber(x.scale.invert(isHorizontal ? 0 : plotHeight)) : 0;
-  const max = x ? toNumber(x.scale.invert(isHorizontal ? plotWidth : 0)) : 0;
   const data = useMemo(() => {
     const data: SeriesPoint[] = [];
-    for (let i = min; i <= max; i += step) {
-      data.push({ x: i, y: getY(i) });
+    if (x) {
+      const isHorizontal = x
+        ? x.position === 'top' || x.position === 'bottom'
+        : undefined;
+      const end = isHorizontal ? plotWidth : plotHeight;
+      const scale = x?.scale;
+      for (let i = 0; i <= end; i += step) {
+        const x = toNumber(scale.invert(i));
+        data.push({
+          x,
+          y: getY(x),
+        });
+      }
+      return data;
     }
-    return data;
-  }, [getY, max, min, step]);
-  if (data.length > 0) {
-    return <LineSeries data={data} {...otherProps} />;
-  }
-  return null;
+    return [{ x: 0, y: 0 }];
+  }, [getY, plotHeight, plotWidth, x]);
+  return <LineSeries data={data} {...otherProps} />;
 }
