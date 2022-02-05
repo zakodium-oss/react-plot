9ae3f464f327c02068529a94268364beb83c9424
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-02-05 22:11:10 +0100

    docs: add spectrum2D example

---
+++ stories/examples/spectrum2d.stories.tsx
@@ -0,0 +1,41 @@
+import { Meta } from '@storybook/react';
+
+import { Axis, LineSeries, Plot } from '../../src';
+import data from '../data/spectrum2d.json';
+import { DEFAULT_PLOT_CONFIG } from '../utils';
+
+export default {
+  title: 'Examples/Spectrum 2D',
+} as Meta;
+
+export function Spectrum2D() {
+  const dataList = data.y.map((yArray) =>
+    yArray.map((y, i) => ({
+      x: data.x[i],
+      y,
+    })),
+  );
+  const lineSeriesList = dataList.map((data, i) => (
+    <LineSeries
+      lineStyle={{ stroke: 'black' }}
+      // eslint-disable-next-line react/no-array-index-key
+      key={i}
+      data={data}
+      xShift={`${i * 5}`}
+      yShift={`${-i * 5}`}
+    />
+  ));
+  return (
+    <Plot {...DEFAULT_PLOT_CONFIG}>
+      {lineSeriesList}
+      <Axis id="x" position="bottom" label="P1" />
+      <Axis
+        id="y"
+        position="left"
+        label="P2"
+        paddingStart={0.1}
+        paddingEnd={0.1}
+      />
+    </Plot>
+  );
+}
