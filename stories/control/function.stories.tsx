936b3f528d06ed86dc79a7255934554feb5e1fe3
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-02-08 13:43:33 +0100

    fix : functionSerie story

--- stories/control/function.stories.tsx
+++ stories/control/function.stories.tsx
@@ -1,5 +1,4 @@
 import { Meta } from '@storybook/react';
-import { useRef, useState } from 'react';
 
 import {
   Annotations,
@@ -8,8 +7,8 @@ import {
   Plot,
   FunctionSeries,
   FunctionSeriesProps,
+  useRectangularZoom,
 } from '../../src';
-import { Rectangle } from '../../src/components/Annotations/Rectangle';
 import { DEFAULT_PLOT_CONFIG } from '../utils';
 
 export default {
@@ -25,87 +24,13 @@ export default {
 function getY(x: number) {
   return 4 * Math.sin(2 * x);
 }
-interface RectanglePositions {
-  position?: {
-    x1: number;
-    y1: number;
-    x2: number;
-    y2: number;
-  } | null;
-  minX: number;
-  maxX: number;
-  minY: number;
-  maxY: number;
-}
 export function Control(
   props: FunctionSeriesProps & { min?: number; max?: number },
 ) {
   const { max, min, ...otherProps } = props;
-  const [{ position, minX, maxX, minY, maxY }, setPositions] =
-    useState<RectanglePositions | null>({
-      position: null,
-      minX: min,
-      maxX: max,
-      minY: undefined,
-      maxY: undefined,
-    });
-  let click = useRef<boolean>(false);
+  const zoom = useRectangularZoom();
   return (
-    <Plot
-      {...DEFAULT_PLOT_CONFIG}
-      onMouseDown={({ coordinates: { x, y } }) => {
-        setPositions({
-          position: {
-            x1: x,
-            y1: y,
-            x2: x,
-            y2: y,
-          },
-          minX,
-          maxX,
-          minY,
-          maxY,
-        });
-        click.current = true;
-      }}
-      onMouseUp={() => {
-        click.current = false;
-        if (position.x1 !== position.x2) {
-          setPositions({
-            position: null,
-            minX: Math.min(position.x1, position.x2),
-            maxX: Math.max(position.x1, position.x2),
-            minY: Math.min(position.y1, position.y2),
-            maxY: Math.max(position.y1, position.y2),
-          });
-        }
-      }}
-      onMouseMove={({ coordinates: { x, y } }) => {
-        if (click.current) {
-          setPositions((positions) => ({
-            ...positions,
-            position: {
-              x1: position ? position.x1 : x,
-              y1: position ? position.y1 : y,
-              x2: x,
-              y2: y,
-            },
-          }));
-        }
-      }}
-      onMouseLeave={() => {
-        setPositions((positions) => ({ ...positions, position: null }));
-        click.current = false;
-      }}
-      onDoubleClick={() => {
-        setPositions({
-          minX: min,
-          maxX: max,
-          minY: undefined,
-          maxY: undefined,
-        });
-      }}
-    >
+    <Plot {...DEFAULT_PLOT_CONFIG}>
       <Legend position="embedded" />
       <FunctionSeries
         getY={getY}
@@ -114,34 +39,9 @@ export function Control(
         yAxis="y"
         label="y=4*sin(2*x)"
       />
-      <Annotations>
-        {position && (
-          <Rectangle
-            color="red"
-            style={{ fillOpacity: 0.2, stroke: 'red' }}
-            x1={position.x1}
-            y1={position.y1}
-            x2={position.x2}
-            y2={position.y2}
-          />
-        )}
-      </Annotations>
-      <Axis
-        min={minX}
-        max={maxX}
-        paddingStart={0.1}
-        paddingEnd={0.1}
-        id="x"
-        position="bottom"
-      />
-      <Axis
-        min={minY}
-        max={maxY}
-        id="y"
-        position="left"
-        paddingStart={0.1}
-        paddingEnd={0.1}
-      />
+      <Annotations>{zoom.annotations}</Annotations>
+      <Axis paddingStart={0.1} paddingEnd={0.1} id="x" position="bottom" />
+      <Axis id="y" position="left" paddingStart={0.1} paddingEnd={0.1} />
     </Plot>
   );
 }
