54069fa8091063ebc3e6d8fd4177d41559fc41d1
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-02-20 17:01:23 +0100

    fix: data & story

--- stories/examples/weatherchart.stories.tsx
+++ stories/examples/weatherchart.stories.tsx
@@ -4,9 +4,10 @@ import {
   Axis,
   BarSeries,
   LineSeries,
-  ParallelAxis,
   Plot,
   RangeSeries,
+  RangeSeriesPoint,
+  SeriesPoint,
 } from '../../src';
 import data from '../data/weather.json';
 import { DEFAULT_PLOT_CONFIG, PlotControllerDecorator } from '../utils';
@@ -17,55 +18,55 @@ export default {
 } as Meta;
 
 export function WeatherChart() {
-  const Series = data.map((d) => {
-    const temperature = d.temperature.map(([x, y]) => ({ x, y }));
-    const rainRange = d.variance_rain.map(([x, y1, y2]) => ({
-      x,
-      y1,
-      y2,
-    }));
-    const tempRange = d.variance_range.map(([x, y1, y2]) => ({
-      x,
-      y1,
-      y2,
-    }));
-    const rain = d.rainfall.map(([x, y]) => ({ x, y }));
-    return [
+  const rain: SeriesPoint[] = [];
+  const temperature: SeriesPoint[] = [];
+  const tempRange: RangeSeriesPoint[] = [];
+  data.forEach((d) => {
+    rain.push(...d.rainfall.map(([x, y]) => ({ x, y })));
+    temperature.push(...d.temperature.map(([x, y]) => ({ x, y })));
+    tempRange.push(
+      ...d.variance_range.map(([x, y1, y2]) => ({
+        x,
+        y1,
+        y2,
+      })),
+    );
+  });
+  return (
+    <Plot {...DEFAULT_PLOT_CONFIG}>
       <BarSeries
-        key={0}
         label="rain"
         data={rain}
         lineStyle={{
           stroke: 'blue',
           strokeWidth: '4px',
         }}
-      />,
+        yAxis="z"
+      />
       <RangeSeries
-        key={1}
-        label="rain Range"
-        data={rainRange}
-        lineStyle={{ fill: 'blue', opacity: '0.5' }}
-      />,
-      <RangeSeries
-        key={2}
         label="temperature Range"
         data={tempRange}
         lineStyle={{ fill: 'red', opacity: '0.5' }}
-      />,
+      />
       <LineSeries
-        key={3}
         label="temperature"
         data={temperature}
         lineStyle={{ stroke: 'red' }}
-      />,
-    ];
-  });
-  return (
-    <Plot {...DEFAULT_PLOT_CONFIG}>
-      {Series.map((Serie) => Serie)}
+      />
+
       <Axis id="x" position="bottom" label="time" scale="time" />
-      <Axis id="y" position="left" label="temperature(C°)" />
-      <ParallelAxis id="y" label="rainfall(F°)" />
+      <Axis
+        id="y"
+        position="left"
+        label="temperature(C°)"
+        displayPrimaryGridLines
+        primaryGridLineStyle={{
+          stroke: 'grey',
+          strokeWidth: '1px',
+          strokeDasharray: 'none',
+        }}
+      />
+      <Axis max={5} min={0} id="z" position="right" label="rainfall(mm/h)" />
     </Plot>
   );
 }
