7ca08927ea06f74522d62dc497ba188ba00ffc38
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-02-26 13:02:42 +0100

    docs: optimise spectrum2d

--- stories/examples/spectrum2d.stories.tsx
+++ stories/examples/spectrum2d.stories.tsx
@@ -14,27 +14,27 @@ export default {
   title: 'Examples/Spectrum 2D',
   decorators: [PlotControllerDecorator],
 } as Meta;
-
+const dataList = data.y.map((yArray) =>
+  yArray.map((y, i) => ({
+    x: data.x[i],
+    y,
+  })),
+);
+const lineSeriesList = dataList
+  .map((data, i) => (
+    <LineSeries
+      lineStyle={{ stroke: 'black', fill: 'white' }}
+      // eslint-disable-next-line react/no-array-index-key
+      key={i}
+      data={data}
+      xShift={`${i * 5}`}
+      yShift={`${-i * 5}`}
+    />
+  ))
+  .reverse();
 export function Spectrum2D() {
   const zoom = useRectangularZoom();
-  const dataList = data.y.map((yArray) =>
-    yArray.map((y, i) => ({
-      x: data.x[i],
-      y,
-    })),
-  );
-  const lineSeriesList = dataList
-    .map((data, i) => (
-      <LineSeries
-        lineStyle={{ stroke: 'black', fill: 'white' }}
-        // eslint-disable-next-line react/no-array-index-key
-        key={i}
-        data={data}
-        xShift={`${i * 5}`}
-        yShift={`${-i * 5}`}
-      />
-    ))
-    .reverse();
+
   return (
     <Plot {...DEFAULT_PLOT_CONFIG}>
       {lineSeriesList}
