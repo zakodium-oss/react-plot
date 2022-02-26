e671df7d234925c57457999c8ae1f5deb198d302
Michaël Zasso <targos@protonmail.com>
2022-02-26 13:12:32 +0100

    docs: add vertical annotations to shift example

--- stories/examples/shift.stories.tsx
+++ stories/examples/shift.stories.tsx
@@ -23,8 +23,10 @@ export function SingleSeriesExample(args: { xShift: number; yShift: number }) {
       <Axis id="x" position="bottom" paddingStart={1} paddingEnd={1} />
       <Axis id="y" position="left" paddingStart={1} paddingEnd={1} />
       <Annotations>
-        <Annotation.Line color="lightblue" y1={0} y2={0} x1="0" x2="100%" />
-        <Annotation.Line color="lightblue" y1={1} y2={1} x1="0" x2="100%" />
+        <Annotation.Line color="lightskyblue" y1={0} y2={0} x1="0" x2="100%" />
+        <Annotation.Line color="lightskyblue" y1={1} y2={1} x1="0" x2="100%" />
+        <Annotation.Line color="lightcoral" y1="0" y2="100%" x1={0} x2={0} />
+        <Annotation.Line color="lightcoral" y1="0" y2="100%" x1={3} x2={3} />
       </Annotations>
     </Plot>
   );
