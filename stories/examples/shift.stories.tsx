7aefeec2ea4d90a9c5169c58f7c3a69ddf40390a
Michaël Zasso <targos@protonmail.com>
2022-03-03 14:35:37 +0100

    add story with string

--- stories/examples/shift.stories.tsx
+++ stories/examples/shift.stories.tsx
@@ -16,10 +16,14 @@ const exampleData = [
   { x: 3, y: 0 },
 ];
 
-export function SingleSeriesExample(args: { xShift: number; yShift: number }) {
+export function WithNumber(args: { xShift: number; yShift: number }) {
   return (
     <Plot {...DEFAULT_PLOT_CONFIG}>
-      <LineSeries data={exampleData} {...args} />
+      <LineSeries
+        data={exampleData}
+        lineStyle={{ stroke: 'green' }}
+        {...args}
+      />
       <Axis id="x" position="bottom" paddingStart={1} paddingEnd={1} />
       <Axis id="y" position="left" paddingStart={1} paddingEnd={1} />
       <Annotations>
@@ -32,4 +36,28 @@ export function SingleSeriesExample(args: { xShift: number; yShift: number }) {
   );
 }
 
-SingleSeriesExample.args = { xShift: 0, yShift: 0 };
+WithNumber.args = { xShift: 0, yShift: 0 };
+WithNumber.storyName = 'With number';
+
+export function WithString(args: { xShift: string; yShift: string }) {
+  return (
+    <Plot {...DEFAULT_PLOT_CONFIG}>
+      <LineSeries
+        data={exampleData}
+        lineStyle={{ stroke: 'green' }}
+        {...args}
+      />
+      <Axis id="x" position="bottom" paddingStart={1} paddingEnd={1} />
+      <Axis id="y" position="left" paddingStart={1} paddingEnd={1} />
+      <Annotations>
+        <Annotation.Line color="lightskyblue" y1={0} y2={0} x1="0" x2="100%" />
+        <Annotation.Line color="lightskyblue" y1={1} y2={1} x1="0" x2="100%" />
+        <Annotation.Line color="lightcoral" y1="0" y2="100%" x1={0} x2={0} />
+        <Annotation.Line color="lightcoral" y1="0" y2="100%" x1={3} x2={3} />
+      </Annotations>
+    </Plot>
+  );
+}
+
+WithString.args = { xShift: '0', yShift: '0' };
+WithString.storyName = 'With string';
