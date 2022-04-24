786394597807a75805b02791345cd9ac4691544c
Wadjih Bencheikh <jw_bencheikh@esi.dz>
2022-04-24 12:04:40 +0100

    chore: rename variable

--- src/components/Axis/HorizontalAxisGridLines.tsx
+++ src/components/Axis/HorizontalAxisGridLines.tsx
@@ -43,8 +43,8 @@ export default function HorizontalAxisGridLines(
 
     if (secondaryGrid) {
       const density = 5;
-      const secondaryTicks = scale?.ticks(primaryTicks.length * density) || [];
-      secondaryTicks.forEach((tick) => {
+      const secondaryGridPosition = scale?.ticks(primaryTicks.length * density) || [];
+      secondaryGridPosition.forEach((tick) => {
         const position = scale?.(tick);
         if (
           !position ||
