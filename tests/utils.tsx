2d9811c8a1596fdae4222933fe43683d7558fb7c
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-07-14 13:40:06 +0100

    test: Plot default value test

---
+++ tests/utils.tsx
@@ -0,0 +1,30 @@
+export const DEFAULT_PLOT_CONFIG = {
+  width: 900,
+  height: 540,
+  seriesViewportStyle: {
+    stroke: 'black',
+    strokeWidth: 0.3,
+  },
+};
+export const data = [
+  {
+    x: 0,
+    y: 10,
+  },
+  {
+    x: 1,
+    y: 12,
+  },
+  {
+    x: 2,
+    y: 14,
+  },
+  {
+    x: 3,
+    y: 16,
+  },
+  {
+    x: 4,
+    y: 18,
+  },
+];
