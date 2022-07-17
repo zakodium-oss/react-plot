04a032f5e7b4ce35666d364d131f5d4ebe4e3d4c
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-07-18 00:18:13 +0100

    test: add simple test

--- tests/Plot.test.tsx
+++ tests/Plot.test.tsx
@@ -1,18 +1,20 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 
-import { LineSeries, Plot } from '../src';
+import { Plot, ScatterSeries } from '../src';
 
 import { DEFAULT_PLOT_CONFIG, data } from './utils';
 
 test.describe('Plot', () => {
-  test('default', async ({ mount }) => {
+  test('simple', async ({ mount }) => {
     const component = await mount(
       <Plot {...DEFAULT_PLOT_CONFIG}>
-        <LineSeries data={data} />
+        <ScatterSeries data={data} />
       </Plot>,
     );
+    const Markers = component.locator('_react=ScatterSeries >> Circle');
     const xAxis = component.locator('_react=Axis[position="bottom"]');
     const yAxis = component.locator('_react=Axis[position="left"]');
+    await expect(Markers).toHaveCount(data.length);
     await expect(xAxis).toBeEnabled();
     await expect(yAxis).toBeEnabled();
   });
