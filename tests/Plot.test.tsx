7ce639b7500af5beb4eb453c5af73091a9d4bde9
wadjih-bencheikh18 <jw_bencheikh@esi.dz>
2022-07-18 01:28:06 +0100

    test: annotations test

--- tests/Plot.test.tsx
+++ tests/Plot.test.tsx
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/experimental-ct-react';
 
-import { Plot, ScatterSeries } from '../src';
+import { Annotations, Plot, ScatterSeries } from '../src';
+import { Line } from '../src/components/Annotations/Line';
 
 import { DEFAULT_PLOT_CONFIG, data } from './utils';
 
@@ -11,11 +12,25 @@ test.describe('Plot', () => {
         <ScatterSeries data={data} />
       </Plot>,
     );
-    const Markers = component.locator('_react=ScatterSeries >> Circle');
+    const markers = component.locator('_react=ScatterSeries >> Circle');
     const xAxis = component.locator('_react=Axis[position="bottom"]');
     const yAxis = component.locator('_react=Axis[position="left"]');
-    await expect(Markers).toHaveCount(data.length);
+    await expect(markers).toHaveCount(data.length);
     await expect(xAxis).toBeEnabled();
     await expect(yAxis).toBeEnabled();
+  });
+  test('annotations', async ({ mount }) => {
+    const component = await mount(
+      <Plot {...DEFAULT_PLOT_CONFIG}>
+        <ScatterSeries data={data} />
+        <Annotations>
+          <Line x1="0" x2="3" y1="0" y2="4" color="red" />
+        </Annotations>
+      </Plot>,
+    );
+    const line = component.locator(
+      'line[stroke="red"][x1="0"][x2="3"][y1="0"][y2="4"]',
+    );
+    await expect(line).toBeEnabled();
   });
 });
