4b60443ead6a402c5592b2510786599d2cf7581e
Wadjih Bencheikh <jw_bencheikh@esi.dz>
2022-12-29 16:55:24 +0100

    test: react-plot on server side (#458)

---
+++ tests/serverside.test.tsx
@@ -0,0 +1,8 @@
+import { test, expect } from '@playwright/experimental-ct-react';
+import { ServerSide } from './utils';
+
+test('should render a plot in server-side mode', async ({ mount }) => {
+  const plot = await mount(<ServerSide />);
+  const html = await plot.innerHTML();
+  expect(html).toContain('svg');
+});
